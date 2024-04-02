d589c0661a81c3da95497925ada324c62782d63b
Yichuan <1758783038@qq.com>
2024-04-02 19:10:17 -0400

    Commit

--- train.py
+++ train.py
@@ -133,6 +133,7 @@ def train_loop(tools, configs, warm_starting,train_writer):
         with autocast():
             # Compute prediction and loss
             print(id_frags_list)
+            print(len(id_frags_list))
             encoded_seq = tokenize(tools, seq_frag_tuple)
             print('encoded_seq.input_ids', encoded_seq['input_ids'].shape)
             # exit(0)
