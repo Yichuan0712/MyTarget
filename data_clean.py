780821ccd38db1b550d3d8f00ed6e4c822e7112c
Yichuan <1758783038@qq.com>
2024-04-07 03:59:56 -0400

    Commit

--- data_clean.py
+++ data_clean.py
@@ -118,8 +118,8 @@ class LocalizationDataset(Dataset):
     def get_neg_samples(self, anchor_idx):
         # filtered_samples = [sample for idx, sample in enumerate(self.samples) if idx != anchor_idx]
         anchor_type_protein = self.samples[anchor_idx][4] #class
-        print(anchor_type_protein)
-        print("*"*16)
+        # print(anchor_type_protein)
+        # print("*"*16)
         if self.hard_neg:
             pass
             # hneg = self.hard_mining(anchor_type_protein) #similiar
@@ -134,8 +134,8 @@ class LocalizationDataset(Dataset):
         for _ in range(self.n_neg):
             chosen_sample = random.choice(all_neg_samples)
             neg_samples.append(chosen_sample)
-            print(chosen_sample[4])
-        print('*'*32)
+            # print(chosen_sample[4])
+        # print('*'*32)
         # exit(0)
 
         neg_samples_with_weight = []
