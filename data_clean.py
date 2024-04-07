53a8a2c6bedf86e779e5821dcdd5a1b84e615f6c
Yichuan <1758783038@qq.com>
2024-04-07 03:41:43 -0400

    Commit

--- data_clean.py
+++ data_clean.py
@@ -50,6 +50,8 @@ class LocalizationDataset(Dataset):
            return len(self.samples)
         
     def __getitem__(self, idx):
+        print('full_list', self.full_list)
+        print('idx of __getitem__', idx)
         if self.apply_supcon and self.mode=="train":
             anchor_type = self.full_list[idx]
             anchor = random.choice(self.class_id[anchor_type])
